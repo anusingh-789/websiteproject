51fc37763d7e40f2d1fc511a751a0efd4849b0ba
anusingh-789 <76813066+anusingh-789@users.noreply.github.com>
2026-04-09 18:50:41 +0530

    Create Schedule.js

---
+++ src/Schedule.js
@@ -0,0 +1,45 @@
+function Schedule() {
+  return (
+    <div>
+
+      <section>
+        <h2>Schedule</h2>
+
+        <table border="1">
+
+          <thead>
+            <tr>
+              <th>Time</th>
+              <th>Session</th>
+              <th>Location</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>10:00 AM</td>
+              <td>Opening Ceremony</td>
+              <td>CS Hall</td>
+            </tr>
+
+            <tr>
+              <td>12:00 PM</td>
+              <td>AI Workshop</td>
+              <td>Lab G012</td>
+            </tr>
+
+            <tr>
+              <td>02:00 PM</td>
+              <td>Cybersecurity Talk</td>
+              <td>Anna Hall</td>
+            </tr>
+          </tbody>
+
+        </table>
+      </section>
+
+    </div>
+  );
+}
+
+export default Schedule;

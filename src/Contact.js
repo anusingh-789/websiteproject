12e087b30c949fedbd48e0d2a08f3f745c7193c6
anusingh-789 <76813066+anusingh-789@users.noreply.github.com>
2026-04-09 18:49:02 +0530

    Create Contact.js

---
+++ src/Contact.js
@@ -0,0 +1,28 @@
+function Contact() {
+  return (
+    <div>
+
+      <section>
+        <h2>Contact Team</h2>
+
+        <address>
+          Innovation Block,<br />
+          VIT University,<br />
+          Vellore - 632014
+        </address>
+        <br></br>
+        <br></br>
+
+        <p>For further queries, feel free to contact us!</p>
+        <br></br>
+
+
+        <p>Email: eventshead@vit.ac.in</p>
+
+      </section>
+
+    </div>
+  );
+}
+
+export default Contact;
